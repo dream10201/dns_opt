cdc42e3b3c41fca520413ade03ff7b0c3575d558
dream10201 <xiuxiu10201@gmail.com>
2024-06-17 17:52:05 +0800

    Update opt.sh

--- opt.sh
+++ opt.sh
@@ -13,11 +13,6 @@ AD_LIST=(
 WHITE_LIST=(
     "https://raw.githubusercontent.com/privacy-protection-tools/dead-horse/master/anti-ad-white-for-smartdns.txt"
 )
-
-ad_tmp=$(mktemp)
-white_tmp=$(mktemp)
-conf_tmp=$(mktemp)
-
 download() {
     echo -n "download ${1}"
     if ! curl -sSx ${PROXY} "${1}" >> "${2}"; then
@@ -26,6 +21,37 @@ download() {
     echo -ne " \033[32m\xE2\x9C\x85\033[0m"
     echo ""
 }
+ad_tmp=$(mktemp)
+white_tmp=$(mktemp)
+conf_tmp=$(mktemp)
+
+other_list(){
+    local v2ray_rules_dat_ad=(
+        "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/reject-list.txt"
+    )
+    local v2ray_rules_dat_white=(
+        "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/direct-list.txt"
+    )
+    local bl=""
+    local wl=""
+    for bl in "${v2ray_rules_dat_ad[@]}"; do
+        echo -n "download ${bl}"
+        if ! curl -sSx ${PROXY} "${bl}" | sed 's/^/address \//;s/$/ \/#/' >> "${ad_tmp}"; then
+            echo  -ne " \033[31m\xE2\x9D\x8E\033[0m"
+        fi
+        echo -ne " \033[32m\xE2\x9C\x85\033[0m"
+        echo ""
+    done
+    for wl in "${v2ray_rules_dat_white[@]}"; do
+        echo -n "download ${bl}"
+        if ! curl -sSx ${PROXY} "${wl}" | sed 's/^/address \//;s/$/ \/-/' >> "${white_tmp}"; then
+            echo  -ne " \033[31m\xE2\x9D\x8E\033[0m"
+        fi
+        echo -ne " \033[32m\xE2\x9C\x85\033[0m"
+        echo ""
+    done
+}
+other_list
 for bl in "${AD_LIST[@]}"; do
     download "$bl" "$ad_tmp"
 done
