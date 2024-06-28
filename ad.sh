dc6423d21f8ab4d5f8cfb90f67338977214c9c46
dream10201 <xiuxiu10201@gmail.com>
2024-06-28 23:40:27 +0800

    Update ad.sh

--- ad.sh
+++ ad.sh
@@ -38,15 +38,15 @@ other_list(){
     local url=""
     for bl in "${v2ray_rules_dat_ad[@]}"; do
         echo -n "download ${bl}"
-        urls=$(curl -sS  "${bl}")
-        for url in ${urls}; do
-            echo "${url}" | { grep -qF -f - "${ad_tmp}" && cat || sed 's/^/0.0.0.0 /;s/$//'; } >> "${ad_hosts_tmp}"
-        done
+        # urls=$(curl -sS  "${bl}")
+        # for url in ${urls}; do
+        #     echo "${url}" | { grep -qF -f - "${ad_tmp}" && cat || sed 's/^/0.0.0.0 /;s/$//'; } >> "${ad_hosts_tmp}"
+        # done
         #if ! curl -sS "${bl}" | sed 's/^/address \//;s/$/\/#/' >> "${ad_hosts_tmp}"; then
         #curl -sS "${bl}" | { grep -qF -f - "${ad_tmp}" && cat || sed 's/^/0.0.0.0 /;s/$//'; } >> "${ad_hosts_tmp}"
-        #if ! curl -sS "${bl}" | sed 's/^/0.0.0.0 /;s/$//' >> "${ad_hosts_tmp}"; then
-            #echo_err
-        #fi
+        if ! curl -sS "${bl}" | sed 's/^/0.0.0.0 /;s/$//' >> "${ad_hosts_tmp}"; then
+            echo_err
+        fi
         echo_success
         echo ""
     done
