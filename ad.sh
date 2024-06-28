ec263ab91beff2e67b727dc2fc7601590c0c485f
dream10201 <xiuxiu10201@gmail.com>
2024-06-28 23:22:35 +0800

    Update ad.sh

--- ad.sh
+++ ad.sh
@@ -37,7 +37,8 @@ other_list(){
     for bl in "${v2ray_rules_dat_ad[@]}"; do
         echo -n "download ${bl}"
         #if ! curl -sS "${bl}" | sed 's/^/address \//;s/$/\/#/' >> "${ad_hosts_tmp}"; then
-        if ! grep -qF "${bl}" "${ad_tmp}" && ! curl -sS "${bl}" | sed 's/^/0.0.0.0 /;s/$//' >> "${ad_hosts_tmp}"; then
+        if ! curl -sS "${bl}" | { grep -qF -f - "${ad_tmp}" && cat || sed 's/^/0.0.0.0 /;s/$//' >> "${ad_hosts_tmp}"; }; then
+        #if ! curl -sS "${bl}" | sed 's/^/0.0.0.0 /;s/$//' >> "${ad_hosts_tmp}"; then
             echo_err
         fi
         echo_success
