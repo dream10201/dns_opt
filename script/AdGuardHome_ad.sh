fbbfa02c3a7ae587015e8fcea5a60f59d8822c8a
dream10201 <xiuxiu10201@gmail.com>
2024-07-18 11:11:03 +0800

    Create AdGuardHome_ad.sh

---
+++ script/AdGuardHome_ad.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# https://www.shellcheck.net/
+# https://www.codeclean.net/tools/bash/
+
+AD_LIST=(
+    "https://anti-ad.net/adguard.txt"
+    "https://adguardteam.github.io/HostlistsRegistry/assets/filter_29.txt"
+)
+ad_tmp=$(mktemp)
+white_tmp=$(mktemp)
+
+download() {
+    echo "download ${1}"
+    local temp=`curl -sS "${1}"`
+    echo ${temp}
+    echo ${temp} | grep "^||" >> ${ad_tmp}
+    echo ${temp} | grep "^@@" >> ${white_tmp}
+}
+
+for bl in "${AD_LIST[@]}"; do
+    download "$bl"
+done
+
+cat smartdns_ad.conf | sed 's/\/\([^\/]*\)\/#/||\1^/' > ${ad_tmp}
+grep "^||" "${ad_tmp}" | sort | uniq >adguard_ad.txt
+grep "^@@" "${white_tmp}" | sort | uniq >adguard_white.txt
+
+rm ${ad_tmp}
+rm ${white_tmp}
+exit 0
